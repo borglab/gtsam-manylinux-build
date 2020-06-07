0cb60bd67d014c57a490c13ed11c3005bfd2a862
Fan Jiang <prof.fan@foxmail.com>
2020-06-06 23:26:35 -0400

    new cpython version

--- build-wheels-new.sh
+++ build-wheels-new.sh
@@ -56,7 +56,7 @@ touch ${PYTHON_LIBRARY}
 
 # FIX auditwheel
 # https://github.com/pypa/auditwheel/issues/136
-cd /opt/_internal/cpython-3.7.6/lib/python3.7/site-packages/auditwheel/
+cd /opt/_internal/cpython-3.7.7/lib/python3.7/site-packages/auditwheel/
 patch -p2 < /io/auditwheel.txt
 cd $CURRDIR
 
