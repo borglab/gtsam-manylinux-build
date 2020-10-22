46c7be98a5ff1d66c5929779901f76be8e7b8b9b
Fan Jiang <i@fanjiang.me>
2020-10-22 14:55:25 -0400

    Fix auditwheel path

--- build-wheels-new.sh
+++ build-wheels-new.sh
@@ -57,7 +57,7 @@ touch ${PYTHON_LIBRARY}
 # FIX auditwheel
 # https://github.com/pypa/auditwheel/issues/136
 echo /opt/_internal/*/*/*/*/auditwheel
-cd /opt/_internal/cpython-3.7.9/lib/python3.7/site-packages/auditwheel/
+cd /opt/_internal/tools/lib64/python3.7/site-packages/auditwheel
 patch -p2 < /io/auditwheel.txt
 cd $CURRDIR
 
