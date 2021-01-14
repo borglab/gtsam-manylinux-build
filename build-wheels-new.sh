68a7c44f8cee4ef066be80d30d357b08f6781035
Varun Agrawal <varagrawal@gmail.com>
2021-01-13 23:41:09 -0500

    Fix Wheels build (#10)
    * use only python version to set wrapper
    
    * get python include directory and library path from the interpreter
    
    * minor fixes
    
    * added docs for Python variables
    
    * testing just python version
    
    * Revert "testing just python version"
    
    This reverts commit 51974b0d7e35e0e40de3ebf75e0b8ad8665ee65e.
    
    * drop support for python 3.5 since EOL reached
    
    * add back python 3.5 support
    
    * use custom docker image
    
    * set CURRDIR correctly
    
    * more efficiency
    
    * minimal cmake so that build is faster
    
    * use full cmake to avoid unnecessary builds

--- build-wheels-new.sh
+++ build-wheels-new.sh
@@ -1,139 +1,72 @@
 #!/bin/bash
-set -x -e
-
-function retry {
-  local retries=$1
-  shift
-
-  local count=0
-  until "$@"; do
-    exit=$?
-    wait=$((2 ** $count))
-    count=$(($count + 1))
-    if [ $count -lt $retries ]; then
-      echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
-      sleep $wait
-    else
-      echo "Retry $count/$retries exited $exit, no more retries left."
-      return $exit
-    fi
-  done
-  return 0
-}
-
-# Install a system package required by our library
-retry 3 yum install -y wget libicu libicu-devel
-
-echo "Current CentOS Version:"
-cat /etc/centos-release
-
-yum install centos-release-scl-rh
-
-retry 3 yum install -y devtoolset-7-gcc-c++
-source /opt/rh/devtoolset-7/enable
-echo "Current GCC version:"
-gcc -v
 
 CURRDIR=$(pwd)
-
-# Build Boost staticly
-mkdir -p boost_build
-cd boost_build
-retry 3 wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz
-tar xzf boost_1_65_1.tar.gz
-cd boost_1_65_1
-./bootstrap.sh --with-libraries=serialization,filesystem,thread,system,atomic,date_time,timer,chrono,program_options,regex
-./b2 -j$(nproc) cxxflags="-fPIC" runtime-link=static variant=release link=static install
-
-cd $CURRDIR
-
-git clone https://github.com/borglab/gtsam.git -b prerelease/4.1.1
-
-ORIGPATH=$PATH
-
-PYTHON_LIBRARY=$(cd $(dirname $0); pwd)/libpython-not-needed-symbols-exported-by-interpreter
-touch ${PYTHON_LIBRARY}
 
 # FIX auditwheel
 # https://github.com/pypa/auditwheel/issues/136
 echo /opt/_internal/*/*/*/*/auditwheel
 cd /opt/_internal/tools/lib64/python3.7/site-packages/auditwheel
 patch -p2 < /io/auditwheel.txt
-cd $CURRDIR
+cd $BUILDDIR
+
+PYBIN="/opt/python/$PYTHON_VERSION/bin"
+PYVER_NUM=$($PYBIN/python -c "import sys;print(sys.version.split(\" \")[0])")
+PYTHONVER="$(basename $(dirname $PYBIN))"
+
+export PATH=$PYBIN:$PATH
+
+${PYBIN}/pip install -r /io/requirements.txt
+
+PYTHON_EXECUTABLE=${PYBIN}/python
+# We use distutils to get the include directory and the library path directly from the selected interpreter
+# We provide these variables to CMake to hint what Python development files we wish to use in the build.
+PYTHON_INCLUDE_DIR=$(${PYTHON_EXECUTABLE} -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
+PYTHON_LIBRARY=$(${PYTHON_EXECUTABLE} -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
+
+echo ""
+echo "PYTHON_EXECUTABLE:${PYTHON_EXECUTABLE}"
+echo "PYTHON_INCLUDE_DIR:${PYTHON_INCLUDE_DIR}"
+echo "PYTHON_LIBRARY:${PYTHON_LIBRARY}"
+echo ""
+
+cmake /gtsam -DCMAKE_BUILD_TYPE=Release \
+    -DGTSAM_BUILD_TESTS=OFF -DGTSAM_BUILD_UNSTABLE=ON \
+    -DGTSAM_USE_QUATERNIONS=OFF \
+    -DGTSAM_BUILD_EXAMPLES_ALWAYS=OFF \
+    -DGTSAM_ALLOW_DEPRECATED_SINCE_V41=OFF \
+    -DCMAKE_INSTALL_PREFIX=$BUILDDIR/../gtsam_install \
+    -DBoost_USE_STATIC_LIBS=ON \
+    -DBOOST_ROOT=/usr/local \
+    -DBoost_NO_SYSTEM_PATHS=ON \
+    -DBUILD_STATIC_METIS=ON \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DGTSAM_BUILD_WITH_MARCH_NATIVE=OFF \
+    -DGTSAM_WITH_TBB=OFF \
+    -DGTSAM_BUILD_PYTHON=ON \
+    -DGTSAM_PYTHON_VERSION=$PYVER_NUM \
+    -DPYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIR \
+    -DPYTHON_LIBRARY=$PYTHON_LIBRARY; ec=$?
+
+if [ $ec -ne 0 ]; then
+    echo "Error:"
+    cat ./CMakeCache.txt
+    exit $ec
+fi
+set -e -x
+
+make -j$(nproc) install
 
 mkdir -p /io/wheelhouse
 
-declare -a PYTHON_VERS=( $1 )
+# "${PYBIN}/pip" wheel . -w "/io/wheelhouse/"
+cd python
 
-# Compile wheels
-for PYVER in ${PYTHON_VERS[@]}; do
-    PYBIN="/opt/python/$PYVER/bin"
-    PYVER_NUM=$($PYBIN/python -c "import sys;print(sys.version.split(\" \")[0])")
-    "${PYBIN}/pip" install -r /io/requirements.txt
-    PYTHONVER="$(basename $(dirname $PYBIN))"
-    BUILDDIR="/io/gtsam_$PYTHONVER/gtsam_build"
-    mkdir -p $BUILDDIR
-    cd $BUILDDIR
-    export PATH=$PYBIN:$ORIGPATH
-    "${PYBIN}/pip" install cmake
-
-    PYTHON_EXECUTABLE=${PYBIN}/python
-    PYTHON_INCLUDE_DIR=$( find -L ${PYBIN}/../include/ -name Python.h -exec dirname {} \; )
-
-    echo ""
-    echo "PYTHON_EXECUTABLE:${PYTHON_EXECUTABLE}"
-    echo "PYTHON_INCLUDE_DIR:${PYTHON_INCLUDE_DIR}"
-    echo "PYTHON_LIBRARY:${PYTHON_LIBRARY}"
-    
-    cmake $CURRDIR/gtsam -DCMAKE_BUILD_TYPE=Release \
-        -DGTSAM_BUILD_TESTS=OFF -DGTSAM_BUILD_UNSTABLE=ON \
-        -DGTSAM_USE_QUATERNIONS=OFF \
-        -DGTSAM_BUILD_EXAMPLES_ALWAYS=OFF \
-        -DGTSAM_INSTALL_CYTHON_TOOLBOX=OFF \
-        -DGTSAM_PYTHON_VERSION=$PYVER_NUM \
-        -DGTSAM_ALLOW_DEPRECATED_SINCE_V41=OFF \
-        -DCMAKE_INSTALL_PREFIX=$BUILDDIR/../gtsam_install \
-        -DBoost_USE_STATIC_LIBS=ON \
-        -DBOOST_ROOT=/usr/local \
-        -DBoost_NO_SYSTEM_PATHS=ON \
-        -DBUILD_STATIC_METIS=ON \
-        -DBUILD_SHARED_LIBS=OFF \
-        -DGTSAM_TYPEDEF_POINTS_TO_VECTORS=ON \
-        -DGTSAM_USE_CUSTOM_PYTHON_LIBRARY=ON \
-        -DGTSAM_BUILD_WITH_MARCH_NATIVE=OFF \
-        -DGTSAM_BUILD_PYTHON=ON \
-        -DPYTHON_EXECUTABLE:FILEPATH=${PYTHON_EXECUTABLE} \
-        -DPYTHON_INCLUDE_DIRS:PATH=${PYTHON_INCLUDE_DIR} \
-        -DPYTHON_LIBRARY:FILEPATH=${PYTHON_LIBRARY}; ec=$?
-
-    if [ $ec -ne 0 ]; then
-        echo "Error:"
-        cat ./CMakeCache.txt
-        exit $ec
-    fi
-    set -e -x
-    
-    make -j$(nproc) install &
-
-    start=$(date +%s)
-    while ps -p $! > /dev/null
-    do
-      sleep 60
-      now=$(date +%s)
-      printf "%d seconds have elapsed\n" $(( (now - start) ))
-    done
-
-    # "${PYBIN}/pip" wheel . -w "/io/wheelhouse/"
-    cd python
-
-    "${PYBIN}/python" setup.py bdist_wheel --python-tag=$PYTHONVER --plat-name=$PLAT
-    cp ./dist/*.whl /io/wheelhouse
-done
+"${PYBIN}/python" setup.py bdist_wheel --python-tag=$PYTHONVER --plat-name=$PLAT
+# cp ./dist/*.whl /io/wheelhouse/
 
 # Bundle external shared libraries into the wheels
-for whl in /io/wheelhouse/*.whl; do
+for whl in ./dist/*.whl; do
     auditwheel repair "$whl" -w /io/wheelhouse/
-    rm $whl
 done
 
 for whl in /io/wheelhouse/*.whl; do
