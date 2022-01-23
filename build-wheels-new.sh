70b5d127396455ae68c3c9eda473a12edcda27c2
John Lambert <johnlambert@gatech.edu>
2022-01-23 00:16:50 -0700

    Update build-wheels-new.sh

--- build-wheels-new.sh
+++ build-wheels-new.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CURRDIR=$(pwd)
-GTSAM_BRANCH="feature/robustTriangulation"
+GTSAM_BRANCH="develop"
 
 # Clone GTSAM
 git clone https://github.com/borglab/gtsam.git -b $GTSAM_BRANCH /gtsam
