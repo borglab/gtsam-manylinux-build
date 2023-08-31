cdfe2885582344a8ec632f7133cfd1bdc4523f4b
John Lambert <johnlambert@gatech.edu>
2023-08-30 22:38:42 -0400

    Update build-macos-new.sh

--- build-macos-new.sh
+++ build-macos-new.sh
@@ -27,9 +27,9 @@ brew update
 brew install wget "$1" cmake || true
 
 CURRDIR=$(pwd)
-GTSAM_BRANCH="release/4.2a8"
-GTSAM_LIB_VERSION="4.2.0"
-GTSAM_PYTHON_VERSION="4.2a8"
+GTSAM_BRANCH="develop"
+GTSAM_LIB_VERSION="4.2a10"
+GTSAM_PYTHON_VERSION="4.2a10"
 
 # Build Boost staticly
 mkdir -p boost_build
